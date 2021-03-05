b50d6935d592857d6a13a177c313a6f9dc27439f
tanya1810 <tanya.sood.311@gmail.com>
2021-03-06 02:13:21 +0530

    changes

--- home/views.py
+++ home/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.shortcuts import render, redirect
 
 def test(request):
-	return render(request, 'home/base.html')
+	return render(request, 'home/index.html')
 # Create your views here.
