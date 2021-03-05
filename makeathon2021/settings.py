91345a3cc8298c94e5db7d5642b22269de2681f8
tanya1810 <tanya.sood.311@gmail.com>
2021-03-06 04:07:08 +0530

    login register

--- makeathon2021/settings.py
+++ makeathon2021/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
     'user.apps.UserConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -87,18 +88,18 @@ DATABASES = {
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    # },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    # },
 ]
 
 
@@ -120,3 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+LOGIN_REDIRECT_URL = 'test'
+LOGOUT_REDIRECT_URL = 'login'
+
+AUTH_USER_MODEL = "user.User"
--- user/views.py
+++ user/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegistrationForm
+from django.contrib import messages
+from django.contrib.auth import get_user_model
+from django.core.mail import send_mail, EmailMessage
+from django.contrib import messages
+from django.http import HttpResponse
+User = get_user_model()
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            if email.find("thapar.edu") == -1:
+                messages.add_message(request, messages.INFO, f'''Please enter thapar's email id''')
+                return redirect('register')
+                    
+            else:
+                User = form.save()
+                return redirect('login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'user/register.html', {'form': form})
+
+
