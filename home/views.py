1ee4d794058c17d8d18d27a340d5b83348c87a65
N15H4N7 <ngupta_be19@thapar.edu>
2021-03-06 02:21:51 +0530

    baki template

--- home/views.py
+++ home/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render, redirect
 
 def test(request):
 	return render(request, 'home/base.html')
+
+def test1(request):
+	return render(request, 'home/portfolio-details.html')
 # Create your views here.
