937a0008c21e68316d409807d24f5067d0e7de4d
tanya1810 <tanya.sood.311@gmail.com>
2021-03-07 04:43:22 +0530

    purchase coupon

--- coupons/views.py
+++ coupons/views.py
@@ -7,12 +7,7 @@ from django.utils.crypto import get_random_string
 # Create your views here.
 def available(request, id=None):
 	user = request.user
-
-	# print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
-	# print(coupon)
-	# print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
 	if id:
-		# print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
 		coupon = AvailableCoupons.objects.get(id=id)
 		if user.coins <= coupon.cost:
 			messages.add_message(request, messages.INFO, 'You don\'t have enough coins to buy.')
@@ -20,16 +15,9 @@ def available(request, id=None):
 			unique_code = get_random_string(length=8)
 
 			Purchased = PurchasedCoupons.objects.create(coupon=coupon, unique_code=unique_code, owner=user)
-			print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
-			print(user.coins)
 			user.coins = user.coins - coupon.cost
-			print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
 			print(user.coins)
-			print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
 			user.save()
-
-
-
 	coupons = AvailableCoupons.objects.order_by('-id')
 	user = request.user
 
@@ -37,3 +25,10 @@ def available(request, id=None):
 		'coupons' : coupons,
 	}
 	return render(request, 'coupons/available_coupons.html', context)
+
+def purchased(request, id=None):
+	coupons = PurchasedCoupons.objects.filter(owner=request.user)
+	context = {
+		'coupons': coupons,
+	}
+	return render(request, 'coupons/purchased_coupons.html', context)
