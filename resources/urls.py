c20944d3922b7dbfd2c785490f44be11184e6763
N15H4N7 <ngupta_be19@thapar.edu>
2021-03-07 00:46:07 +0530

    Buy Coupon

--- resources/urls.py
+++ resources/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('new/resource/', post_resource, name='post_resource'),
     path('like/<int:pk>/', like_resource, name='like_resource'),
     path('dislike/<int:pk>/', dislike_resource, name='like_resource'),
-    path('update/resource/<int:pk>', update_resource, name='update_resource'),
+    path('update/resource/<int:pk>/', update_resource, name='update_resource'),
+    path('delete/<int:pk>/', delete_resource, name='delete_resource'),
+    path('my/posted/resources/', my_posted_resources, name='my_posted_resources'),
+    path('my/bought/resources/', my_bought_resources, name='my_bought_resources'),
 ]
