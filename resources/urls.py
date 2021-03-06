158fc192b0224d2c4eca8bb31b785025e4f36bdc
N15H4N7 <ngupta_be19@thapar.edu>
2021-03-06 23:31:43 +0530

    Available Coupons

--- resources/urls.py
+++ resources/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from .  import views
 
 urlpatterns = [
-    path('resources/', all_resources, name='all-resources'),
-    path('new/resource/', post_resource, name='post_resource'),
+    path('resources/', views.all_resources, name='all-resources'),
+    path('new/resource/', views.post_resource, name='post_resource'),
 ]
