680a0b34250984379d6e40daad2a4c52b553e04f
tanya1810 <tanya.sood.311@gmail.com>
2021-03-07 00:51:26 +0530

    pdf preview

--- makeathon2021/settings.py
+++ makeathon2021/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'crispy_forms',
 ]
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
