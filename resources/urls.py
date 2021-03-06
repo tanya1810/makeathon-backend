b114b82e108f85ebae3632e16edefa5bbeb2888a
tanya1810 <tanya.sood.311@gmail.com>
2021-03-07 00:21:31 +0530

    resource page

--- resources/urls.py
+++ resources/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import *
+from . import views
 
 urlpatterns = [
     path('resources/', views.all_resources, name='all-resources'),
