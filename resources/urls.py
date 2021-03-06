61b38f7e8ccd548cc843f1a3b5e7ee4c4ef3522a
Kajal <kajal11122000@gmail.com>
2021-03-06 17:20:56 +0530

    Post a resource added

--- resources/urls.py
+++ resources/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('resources/', all_resources, name='all-resource'),
+    path('resources/', all_resources, name='all-resources'),
+    path('new/resource/', post_resource, name='post_resource'),
 ]
