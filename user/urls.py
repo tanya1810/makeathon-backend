4d8f100cfcc15ed328f8a71edec6a5bdf4c762b3
Kajal <kajal11122000@gmail.com>
2021-03-07 01:21:10 +0530

    user profile added

--- user/urls.py
+++ user/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('register/',            views.register,     name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name = 'login'),
+    path('', auth_views.LoginView.as_view(template_name='user/login.html'), name = 'login'),
     path('logout/', 		auth_views.LogoutView.as_view(),		name='logout'),
     
 ]
