a50c985b73b00c02e26c7a6ef0dac3010e8ca466
Kajal <kajal11122000@gmail.com>
2021-03-07 10:46:24 +0530

    delete comment

--- home/urls.py
+++ home/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('my/feeds/', 							my_post, 			name='my_feeds'),
     path('delete/feed/<int:id>/', 				delete_feed,		name='delete_feed'),
     path('update/feed/<int:id>/', 				update_feed,		name='update_feed'),
+    path('delete/comment/<int:pk>/',            delete_comment,     name='delete-comment'),
 ]
