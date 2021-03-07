3aa6203cfa6545a3124a971417f22414e35bded7
Kajal <kajal11122000@gmail.com>
2021-03-07 05:47:32 +0530

    update/delete feed

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
+from .views import *
 
 urlpatterns = [
-    path('feeds/',				views.feeds_home,		name='feeds'),
+    path('feeds/', feeds_home, name='feeds'),
+    path('delete/feed/<int:id>/', delete_feed, name='delete_feed'),
+    path('update/feed/<int:id>/', update_feed, name='update_feed'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import *
 from user.models import User
-from .forms import AnswerForm
+from .forms import *
 
 def feeds_home(request):
 	print('hello')
@@ -18,3 +18,28 @@ def feeds_home(request):
 		'feeds'	 : feeds,
 	}
 	return render(request, 'home/feeds.html', context)
+
+def delete_feed(request, pk):
+	feed = Feed.objects.get(id=pk)
+	if(request.user == feed.author):
+		feed.delete()
+	else:
+			messages.add_message(request, messages.INFO, 'You are not authorized to delete this feed.')
+
+	return redirect('feeds')
+
+def update_feed(request, pk):
+	feed = Feed.objects.get(id=pk)
+	if request.method == 'POST':
+		if(request.user == feed.author):
+			form = UpdateFeedForm(request.POST, request.FILES, instance=feed)
+			if form.is_valid():
+				form.save()
+				return redirect('feeds')
+		else:
+			messages.add_message(request, messages.INFO, 'You are not authorized to edit this feed.')
+	form = UpdateFeedForm(instance=feed)
+	context = {
+		'form' : form,
+	}
+	return render(request, 'home/update_feed.html', context)
